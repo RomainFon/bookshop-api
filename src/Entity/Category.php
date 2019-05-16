cfcb21bfb33cd9c48cbf2bc192ee4b2e777a2789
RomainFon <romain.fon974@gmail.com>
2019-05-16 12:01:18 +0200

    Commit seance 16/05

---
+++ src/Entity/Category.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A book.
+ *
+ * @ORM\Entity
+ * @ApiResource
+ */
+class Category
+{
+    /**
+     * @var int The id of this category.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The name of this category.
+     *
+     * @ORM\Column(type="string")
+     */
+    public $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function addProduct(Product $product){
+        $product->category = $this;
+        $this->producs->add($product);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param mixed
+     */
+    public function setProducts($products)
+    {
+        $this->products = $products;
+    }
+
+
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A book.
+ *
+ * @ORM\Entity
+ * @ApiResource
+ */
+class Product
+{
+    /**
+     * @var int The id of this product.
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The name of this product.
+     *
+     * @ORM\Column(type="string")
+     */
+    public $name;
+
+    /**
+     * @var string The description of this product.
+     *
+     * @ORM\Column(type="string")
+     */
+    public $description;
+
+    /**
+     * @var string The price of this product.
+     *
+     * @ORM\Column(type="float")
+     */
+    public $price;
+
+    /**
+     * @var string The stock of this product.
+     *
+     * @ORM\Column(type="integer")
+     */
+    public $stock;
+
+    /**
+     * @var string The picture of this product.
+     *
+     * @ORM\Column(type="string")
+     */
+    public $picture;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    public $category;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @param mixed
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @param mixed
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    /**
+     * @param mixed
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    /**
+     * @param mixed
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStock()
+    {
+        return $this->stock;
+    }
+    /**
+     * @param mixed
+     */
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+    /**
+     * @param mixed
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    /**
+     * @param mixed
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+}
